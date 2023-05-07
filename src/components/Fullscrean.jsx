fca181a7cbeb91e439b4afe81282e1fb06624afb
SayFu11a <sgishlakaev@mail.ru>
2023-05-07 19:31:55 +0300

    init

---
+++ src/components/Fullscrean.jsx
@@ -0,0 +1,16 @@
+function Fullscrean() {
+  return (
+    <section className="fullscreen">
+      <div className="fullscreen__body">
+        <a href="#fresh" class="fullscreen__link">
+          <img
+            src="https://af.attachmail.ru/cgi-bin/readmsg/butt.png?x-email=sgishlakaev@mail.ru&rid=1358298587100300510729478078642016958939&&id=16834731080781699554%3B0%3B1&project=cloud&x-email=sgishlakaev%40mail.ru"
+            alt=""
+          />
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Fullscrean;
