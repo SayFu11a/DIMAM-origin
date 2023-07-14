829f26e2eede28b1c0980966aef9c35525e1ec40
SayFu11a <sgishlakaev@mail.ru>
2023-07-14 19:15:32 +0300

    search&pagination

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ import NotFound from './pages/NotFound';
 import './scss/app.scss';
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+
   return (
     <div className="wrapper">
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home searchValue={searchValue} setSearchValue={setSearchValue} />}
+        />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,16 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import ClothingBlock from '../components/ClothingBlock';
 import Skeleton from '../components/ClothingBlock/Skeleton';
+import Pagination from '../components/Pagination';
+import Search from '../components/Search';
 
 import Fullscrean from '../components/Fullscrean';
 
-const Home = () => {
+const Home = ({ searchValue, setSearchValue }) => {
   const [clothes, setСlothes] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [typeSort, setTypeSort] = React.useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -18,16 +21,23 @@ const Home = () => {
 
   React.useEffect(() => {
     setIsLoading(true);
+
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
     const sort = typeSort.sortProperty;
 
-    fetch(`https://646888d4e99f0ba0a826b93b.mockapi.io/clothes?${category}&sortBy=${sort}`)
+    fetch(
+      `https://646888d4e99f0ba0a826b93b.mockapi.io/clothes?page=${currentPage}&limit=4&${category}&sortBy=${sort}${search}`,
+    )
       .then((res) => res.json())
       .then((arr) => {
         setСlothes(arr);
         setIsLoading(false);
       });
-  }, [typeSort, categoryId]);
+  }, [typeSort, categoryId, searchValue, currentPage]);
+
+  const closes = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const skeletons = clothes.map((clothe) => <ClothingBlock key={clothe.id} {...clothe} />);
 
   return (
     <>
@@ -36,14 +46,16 @@ const Home = () => {
         <div className="container">
           <div className="content__top">
             <Categories value={categoryId} onClickCatigory={(i) => setCategoryId(i)} />
+            <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+
             <Sort activeSort={typeSort} setActiveSort={setTypeSort} />
           </div>
-          <h2 className="content__title">Весь ассортимент</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : clothes.map((clothe) => <ClothingBlock key={clothe.id} {...clothe} />)}
+          <div className="serch-title_block">
+            <h2 className="content__title">Весь ассортимент </h2>
           </div>
+
+          <div className="content__items">{isLoading ? closes : skeletons}</div>
+          <Pagination setCurrentPage={setCurrentPage} />
         </div>
       </div>
     </>
